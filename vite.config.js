da4f51b9c001f6e4ce2eb03ebc8eacf1b5a5f281
lidengkedev <lidengke0221@126.com>
2021-12-10 15:06:21 +0800

    移除env环境变量配置文件

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,33 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-const host = process.env.VITE_APP_HOST
-const port = process.env.VITE_APP_PORT
-const base = process.env.VITE_APP_BASE
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base,
-  plugins: [vue()],
-  server: {
-    host,
-    port
-  },
-  preview: {
-    host,
-    port
-  },
-  build: {
-    outDir: 'release',
-    manifest: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        example: resolve(__dirname, 'example/indexedDB.html')
+export default defineConfig(({ command, mode }) => {
+  let base = '/',
+    host = 'localhost',
+    port = 9000
+  if (command === 'build' && mode === 'production') {
+    base = 'https://lidengkedev.github.io/example/release/'
+  }
+  return {
+    base,
+    plugins: [vue()],
+    server: {
+      host,
+      port
+    },
+    preview: {
+      host,
+      port
+    },
+    build: {
+      outDir: 'release',
+      manifest: false,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          'example/indexedDB': resolve(__dirname, 'example/indexedDB.html')
+        }
       }
     }
   }
