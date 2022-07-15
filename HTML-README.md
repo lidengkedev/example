034bf0e22d80fda851892cecacd28cd785f5394d
lidengke <lidengke0221@126.com>
2022-07-15 15:15:14 +0800

    Update

--- HTML-README.md
+++ HTML-README.md
@@ -2,5 +2,5 @@
 # HTML 示例文件访问链接汇总目录
 [TOC]
 ## HTML 文件示例
-- [使用 Canvas 绘制一个类似于 Echarts 的柱状图](https://lidengkedev.github.io/example/htmls/2170ecff-e93d-4dd7-922a-a8377a7bbdb9.html)
-- [在输入框内输入文本，输出打字机效果](https://lidengkedev.github.io/example/htmls/4dfd3eba-8778-4a24-ac8b-fcdd6a95131f.html)
+- [使用 Canvas 绘制一个类似于 Echarts 的柱状图](https://lidengkedev.github.io/example/htmls/canvas-bar.html)
+- [在输入框内输入文本，输出打字机效果](https://lidengkedev.github.io/example/htmls/font-print.html)
--- publish.js
+++ publish.js
@@ -1,23 +1,10 @@
 /**
  * 1. 新建一个文件夹 htmls，用于存放一些原生的 html
  * 2. 获取 /dist/htmls 文件夹中所有的文件，并复制到 /dist/htmls 文件夹中
- * 3. 把 /dist/htmls 文件夹中所有的 html 的文件名改为 uuid
- * 4. 获取 /dist/htmls 中每个 html 文件中的 title 的内容和文件名，并保存在 /HTML-README.md 文件中
+ * 3. 获取 /dist/htmls 中每个 html 文件中的 title 的内容和文件名，并保存在 /HTML-README.md 文件中
  */
 const path = require('path')
 const fs = require('node:fs')
-
-/**
- * 生成一个 uuid 值
- * @returns uuid
- */
-function createUuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        const r = Math.random() * 16 | 0
-        const v = c == 'x' ? r : (r & 0x3 | 0x8)
-        return v.toString(16)
-    })
-}
 
 /**
  * 获取绝对路径
@@ -72,17 +59,14 @@ folder.forEach((filename) => {
         fs.mkdirSync(resolve('dist/htmls/'))
     }
 
-    // 生成一个 uuid
-    const uuid = createUuid()
-
     // 复制文件到 /dist/htmls/ 文件夹中
-    fs.copyFileSync(resolve('htmls/' + filename), resolve('dist/htmls/' + uuid + '.html'))
+    fs.copyFileSync(resolve('htmls/' + filename), resolve('dist/htmls/' + filename))
 
     // 读取 HTML-README.md 文件，返回 HTML-README.md 文件的文本内容
     let htmlReadmeMdFileSteam = fs.readFileSync(resolve('HTML-README.md'), { encoding: 'utf-8' })
 
     // 追加 HTML-README.md 文本内容
-    htmlReadmeMdFileSteam = htmlReadmeMdFileSteam + `- [${newFileName}](https://lidengkedev.github.io/example/htmls/${uuid}.html)\n`
+    htmlReadmeMdFileSteam = htmlReadmeMdFileSteam + `- [${newFileName}](https://lidengkedev.github.io/example/htmls/${filename})\n`
     
     // 写入文本内容
     fs.writeFileSync(resolve('HTML-README.md'), htmlReadmeMdFileSteam)
